666274fa4113f794709f61cf77715f1d7d714e2e
Jiafeng Ge <mackondy@gmail.com>
2021-06-19 13:47:44 -0700

    update each one in loop

--- shell_update.py
+++ shell_update.py
@@ -2,13 +2,10 @@ from subprocess import call
 
 call("python -m pip install --upgrade pip")
 
-pkgs = []
 with open("requirements.txt") as f:
     for line in f:
         line_stripped = line.strip()
         if not line_stripped:
             continue
         pkg_name, version = line.split("==")
-        pkgs.append(pkg_name)
-        
-call("pip install --upgrade " + " ".join(pkgs), shell=True)
+        call(f"pip install --upgrade {pkg_name}", shell=True)
