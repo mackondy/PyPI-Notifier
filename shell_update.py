4f0e072e9c30da9829df8df9a0963f302e8877bf
Jiafeng Ge <mackondy@gmail.com>
2021-07-29 08:34:02 -0700

    capture result

--- shell_update.py
+++ shell_update.py
@@ -1,6 +1,14 @@
-from subprocess import call
+from subprocess import run
 
-call("python -m pip install --upgrade pip")
+def pip_update_pkg(pkg_name: str):
+    result = run(f"pip install --upgrade {pkg_name}", shell=True, capture_output=True, text=True)
+    if result.stdout.startswith("Requirement already satisfied"):
+        return
+    print(result.stdout)
+    if pkg_name == 'playwright':
+        run('python -m playwright install')
+
+run("python -m pip install --upgrade pip")
 
 with open("requirements.txt") as f:
     for line in f:
@@ -8,6 +16,4 @@ with open("requirements.txt") as f:
         if not line_stripped:
             continue
         pkg_name, version = line.split("==")
-        call(f"pip install --upgrade {pkg_name}", shell=True)
-
-# call('python -m playwright install')
+        pip_update_pkg(pkg_name)
